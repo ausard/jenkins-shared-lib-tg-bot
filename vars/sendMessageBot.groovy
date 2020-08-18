f049e6922b05b8ddb6336f7e7e54d8967c5a5cee
ausard <ausard@yandex.ru>
2020-08-18 23:55:59 +0300

    update function

--- vars/sendMessageBot.groovy
+++ vars/sendMessageBot.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
-  sh 'curl http://localhost:7001/I%20am%20alive!!!'
+def call(String name = 'I%20am%20alive!') {
+  sh "curl http://localhost:7001/${message}"
 }
