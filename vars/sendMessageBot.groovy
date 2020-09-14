cddc9d0b4d3673fd495468156111dbebb0bb520e
Aleksei Sabetski <ausard@yandex.ru>
2020-09-14 16:15:17 +0300

    Update sendMessageBot.groovy

--- vars/sendMessageBot.groovy
+++ vars/sendMessageBot.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
-def call(String message = 'I%20am%20alive!') {
-  sh "curl 'http://localhost:7001/${message}'"
+def call(String ip, String message = 'I%20am%20alive!') {
+  sh "curl 'http://${ip}:7001/${message}'"
 }
